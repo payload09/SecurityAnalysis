6fc6afa7335af4cc6d24482e9211e84092ac1dd9
arturo <anegreiross@outlook.com>
2021-03-17 21:53:19 -0500

    ok

--- Web_capturer.py
+++ Web_capturer.py
@@ -54,36 +54,7 @@ class Web:
                     img = Image.open(requests.get(x.attrs["src"], stream = True).raw)
                     img.save("{}/images/{}.png".format(path,str(counter)))
                     counter += 1
-            #img = Image.open(requests.get(image_to_find['src'], stream=True).raw)
-            #img.save("{}/images/imagen1.png".format(path))
             
-            
-            #print(image_to_find.attrs["src"])
-        
-            """headers = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) ' 
-                        'AppleWebKit/537.11 (KHTML, like Gecko) '
-                        'Chrome/23.0.1271.64 Safari/537.11',
-                        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
-                        'Accept-Charset': 'ISO-8859-1,utf-8;q=0.7,*;q=0.3',
-                        'Accept-Encoding': 'none',
-                        'Accept-Language': 'en-US,en;q=0.8',
-                        'Connection': 'keep-alive'}
-
-            if os.path.isdir("{}/images".format(path)) is False:
-                os.makedirs("{}/images".format(path))
-
-            request = Request(url = url, headers = headers)
-
-            with closing(urlopen(request).read()) as url_open:
-                self.base = BeautifulSoup(url_open, "html.parser")
-                
-                img = self.base.findAll("img")
-                #img_path = img["src"]
-                #print(img_path)
-                for x in img:
-                    print(x["src"])
-                    #image = Image.open(requests.get(img_path, stream=True).raw)
-                    #image.save(str(img_path))"""
         except Exception as e:
             print("Error by: ",str(e))
 
@@ -107,9 +78,7 @@ class Web:
 
 if __name__ == '__main__':
 
-    #path = input("name> ")
-    path = "arturo"
+    path = input("name> ")
     web_capturer = Web()
     web_capturer.showing_arguments(path)
-    #web_capturer.download_images("wherever", path)
 
