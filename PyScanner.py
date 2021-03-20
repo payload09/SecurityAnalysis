3e7ead240b0c93c93c524ca193d574f4eee433a5
arturo <anegreiross@outlook.com>
2021-03-19 23:52:40 -0500

    working for generate reverse shell to metasploitable

--- PyScanner.py
+++ PyScanner.py
@@ -4,33 +4,45 @@
 import socket
 import sys
 import getopt
-import os 
+import os
 from IPy import IP
+
+
+
+def init_variables():
+    global payload
+    global path
+
+# initializr this funciton to get the global variables in whole the code.
+init_variables()
+
+
 
 
 # this will be contain the value for use on bufferoverflow
 payload = ''
-
+path = "/usr/bin/bash" # path to reverse shell
 def check_ip(ip):
-    
+
     try:
         IP(ip)
         return ip
     except ValueError as e:
+
         return socket.gethostbyname(ip)
 
 
-def init_scanner(host, port):
-    
+def show_port_opened(host, port):
+
     host_name = check_ip(host)
-    
+
     try:
-        
+
         sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        
+
         try:
             sock.connect((host_name, port))
-            print("connected at %s : %s ..!"%(host_name,port))
+            print("target %s  and port open %s"%(host_name,port))
         except Exception:
             pass
             #print("can't open the port")
@@ -48,7 +60,7 @@ def init_process():
 
             if o in ('-s','--scanning'):
                 for x in range(1,1000):
-                    init_scanner(a, x)
+                    show_port_opened(a, x)
     except Exception as e:
         print(str(e))
 
